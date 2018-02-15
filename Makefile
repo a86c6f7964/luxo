c6e5234b5bcb97c8d1976d62579974b31225d662
Cameron Davison <cameron@novilabs.com>
2018-02-14 20:54:07 -0600

    bench

--- Makefile
+++ Makefile
@@ -5,6 +5,13 @@ info:
 
 devdeps:
 	rustup component add rustfmt-preview --toolchain=nightly
+	command -v hyperfine || cargo install hyperfine
+
+release:
+	cargo build --release
+
+bench: release
+	hyperfine --prepare 'sync && sudo purge' 'target/release/luxo example /tmp/luxo/'
 	
 fmt:
 	cargo +nightly fmt
--- src/lib.rs
+++ src/lib.rs
@@ -6,25 +6,27 @@ pub fn stats(folder: &String) {
 }
 
 pub fn example(folder: &String) {
+    println!("open folder [{}]", folder);
     let luxo = luxo::open_with_folder(folder).expect(&format!("unable to open [{}]", folder));
 
-    for i in 1..20 {
-        let written = luxo.write(
+    for i in 1..2000 {
+        let _written = luxo.write(
             format!("test{}", i).as_bytes(),
             &mut format!("value {}", i).as_bytes(),
         ).expect("unable to write");
 
-        println!("able to write [test{}] length {}", i, written)
+        //println!("able to write [test{}] length {}", i, written)
     }
 
-    for i in 1..20 {
+    for i in 1..2000 {
         let mut buf = luxo.read(format!("test{}", i).as_bytes())
             .expect("unable to find buffer");
 
         let mut value = String::new();
-        let num = buf.read_to_string(&mut value)
+        let _num = buf.read_to_string(&mut value)
             .expect("unable to read to end");
 
-        println!("able to read [test{}] of L:{}[{}]", i, num, value)
+        //println!("able to read [test{}] of L:{}[{}]", i, num, value)
     }
+    println!("done reading/writing")
 }
