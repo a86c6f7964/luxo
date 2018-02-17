a8cf3788cec26348ce9f418e5e10cdea159b7313
Cameron Davison <cameron@novilabs.com>
2018-02-17 16:28:44 -0600

    changed up how we read/write stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,70 @@
 mod luxo;
+use std::time::Instant;
+use std::time::Duration;
 use std::io::Read;
 
 pub fn stats(folder: &String) {
     println!("stats {}", folder)
 }
 
+trait Millis {
+    fn as_millis(&self) -> u64;
+}
+
+impl Millis for Duration {
+    fn as_millis(&self) -> u64 {
+        return (self.as_secs() * 1_000) + (self.subsec_nanos() / 1_000_000) as u64;
+    }
+}
+
 pub fn example(folder: &String, store: &String) {
     println!("open folder [{}]", folder);
     let luxo = match store.as_ref() {
-        "simple" => Ok(luxo::open_simple(folder)
-            .expect(&format!("unable to open [{}/{}]", folder, store))),
+        "simple" => {
+            Ok(luxo::open_simple(folder).expect(&format!("unable to open [{}/{}]", folder, store)))
+        }
         _ => Err(format!("unknown store [{}]", store)),
     }.unwrap();
 
-    for i in 1..2000 {
-        let _written = luxo.write(
-            format!("test{}", i).as_bytes(),
-            &mut format!("value {}", i).as_bytes(),
-        ).expect("unable to write");
-
-        //println!("able to write [test{}] length {}", i, written)
+    let now = Instant::now();
+    let num_keys = 2000;
+    let mut keys: Vec<Vec<u8>> = Vec::with_capacity(num_keys);
+    let mut values: Vec<Vec<u8>> = Vec::with_capacity(num_keys);
+    for i in 1..num_keys {
+        let key = format!("test{}", i);
+        let value = format!("value {}", i);
+        keys.push(key.into_bytes());
+        values.push(value.into_bytes());
     }
 
-    for i in 1..2000 {
-        let mut buf = luxo.read(format!("test{}", i).as_bytes())
-            .expect("unable to find buffer");
+    println!("took {}ms to build the strings", now.elapsed().as_millis());
 
-        let mut value = String::new();
-        let _num = buf.read_to_string(&mut value)
-            .expect("unable to read to end");
-
-        //println!("able to read [test{}] of L:{}[{}]", i, num, value)
+    for i in 0..num_keys - 1 {
+        if let Some(key) = keys.get(i) {
+            if let Some(value) = values.get(i) {
+                luxo.write(key, &mut &value[..]).expect("unable to write");
+            }
+        } else {
+            panic!("unable to find key #{}", i)
+        }
     }
-    println!("done reading/writing")
+
+    println!("took {}ms to write key/vals", now.elapsed().as_millis());
+    let now = Instant::now();
+
+    for i in 0..num_keys - 1 {
+        if let Some(key) = keys.get(i) {
+            let mut buf = luxo.read(key).expect("unable to find buffer");
+            let mut value = Vec::new();
+            buf.read_to_end(&mut value).expect("unable to read to end");
+            assert_eq!(value[..], values.get(i).expect("unable to find value")[..])
+        } else {
+            panic!("unable to find key #{}", i)
+        }
+    }
+
+    println!(
+        "took {}ms to read and assert key/vals",
+        now.elapsed().as_millis()
+    );
 }
