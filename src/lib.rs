d7647d92fa364e74b4fcdcdd09bca0a9c0fc0658
Cameron Davison <cameron@novilabs.com>
2018-02-18 20:08:12 -0600

    pull out simple

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::{io, result, str};
-use std::io::{copy, BufRead, BufReader, Read, Write};
-use std::path::{Path, PathBuf};
+use std::io::{BufRead, BufReader, Read};
+mod simple;
+pub use simple::open_simple;
 
 #[derive(Debug)]
 pub enum Error {
@@ -29,60 +30,4 @@ pub trait Luxo<R: Read> {
 
 pub fn stats(folder: &String) {
     println!("stats {}", folder)
-}
-
-use std::fs;
-use std::fs::File;
-
-pub fn open_simple(folder: &String) -> Result<Box<Luxo<File>>> {
-    let path = Path::new(folder);
-    if !path.is_dir() {
-        fs::create_dir(&path)?;
-    }
-
-    let path = fs::canonicalize(path)?;
-    Ok(Box::new(SimpleLuxo { folder: path }))
-}
-
-#[derive(Debug)]
-struct SimpleLuxo {
-    folder: PathBuf,
-}
-
-impl Luxo<File> for SimpleLuxo {
-    // https://bryce.fisher-fleig.org/blog/strategies-for-returning-references-in-rust/index.html
-    fn read(&self, key: &[u8]) -> Result<BufReader<File>> {
-        let k = str::from_utf8(&key)?;
-        let mut key_path = self.folder.to_path_buf();
-        key_path.push(format!("{}.key", k));
-
-        let file = File::open(key_path)?;
-        let reader = BufReader::new(file);
-        Ok(reader)
-    }
-
-    fn write(&self, key: &[u8], value: &mut BufRead) -> Result<u64> {
-        let k = str::from_utf8(&key)?;
-
-        let mut temp_path = self.folder.to_path_buf();
-        temp_path.push(format!("{}.key.tmp", k));
-        let mut end_path = self.folder.to_path_buf();
-        end_path.push(format!("{}.key", k));
-
-        let len;
-        {
-            let mut file = File::create(temp_path.as_path())?;
-            len = copy(value, &mut file)?;
-            file.flush()?;
-            file.sync_all()?;
-        }
-
-        // out of scope so closed
-        fs::rename(temp_path, end_path)?;
-
-        // todo: folder fsync
-        // https://lwn.net/Articles/457667/
-
-        Ok(len)
-    }
 }
