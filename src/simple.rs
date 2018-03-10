4fd0ddf3c92a9143c299ef4a802eb12807751baa
Cameron Davison <cameron@novilabs.com>
2018-03-10 14:30:20 -0600

    hold file lock for duration of run

--- src/simple.rs
+++ src/simple.rs
@@ -10,6 +10,9 @@ use std::io::copy;
 use std::io::Write;
 use str::from_utf8;
 
+extern crate fs2;
+use self::fs2::FileExt;
+
 pub fn open_simple(folder: &String) -> Result<Box<Luxo>> {
     let path = Path::new(folder);
     if !path.is_dir() {
@@ -17,15 +20,29 @@ pub fn open_simple(folder: &String) -> Result<Box<Luxo>> {
     }
 
     let path = fs::canonicalize(path)?;
-    Ok(Box::new(SimpleLuxo { folder: path }))
+    Ok(Box::new(SimpleLuxo::open(path)))
 }
 
 #[derive(Debug)]
 struct SimpleLuxo {
     folder: PathBuf,
+    lock_file: File,
+}
+
+impl SimpleLuxo {
+    fn open(folder: PathBuf) -> SimpleLuxo {
+        let mut key_path = folder.to_path_buf();
+        key_path.push(".lock");
+        let key_path_str: String = key_path.to_str().expect(&format!("unable to create lock string")).to_owned();
+        let lock_file: File = File::create(key_path).expect(&format!("unable to open lock [{}]", key_path_str));
+        lock_file.try_lock_exclusive().expect(&format!("unable to lock [{}]", key_path_str));
+
+        SimpleLuxo { folder, lock_file }
+    }
 }
 
 impl Luxo for SimpleLuxo {
+
     // https://bryce.fisher-fleig.org/blog/strategies-for-returning-references-in-rust/index.html
     fn read(&self, key: &[u8], read_value: &Fn(&mut Read) -> usize) -> Result<Option<usize>> {
         let k = from_utf8(&key)?;
